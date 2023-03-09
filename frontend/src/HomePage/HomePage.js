287799eb55e5e21c3796bcd7250d75947594d530
opvs <samvmvniz@gmail.com>
2023-03-09 15:24:21 -0300

    implementado pastas e arquivos

---
+++ frontend/src/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export default function HomePage() {
+  
+
+  return (
+    <main>
+      <div>oi</div>
+    </main>
+  );
+}
---
+++ frontend/src/routes/coordinator.js
@@ -0,0 +1,4 @@
+export const goToHomePage = (navigate) => {
+  navigate("/");
+};
+
